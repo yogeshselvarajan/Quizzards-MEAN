97b929a34b68076556ae31803288fe1014bc4c35
Yogesh S <yogeshselvarajan@gmail.com>
2023-03-26 17:23:29 +0530

    Committed the final project

---
+++ config/database.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://mongoinst:4bmNU17MfzarNlbh84Xc4I8DNcxkxn7uXd0V54m1tC09PXwGozWnaxNzFAyNoe4XI7W8eRWqFaWPACDb7z77PQ%3D%3D@mongoinst.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@mongoinst@';
+
+
+mongoose.connect(dbURI, {
+  autoIndex: true,
+  useCreateIndex: true,
+});
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+// process.on('SIGTERM', function() {
+//   gracefulShutdown('Heroku app termination', function() {
+//     process.exit(0);
+//   });
+// });
+
+// BRING IN YOUR SCHEMAS & MODELS
+require('../models/User');
+require('../models/Quiz');
+require('../models/Challenge');
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+require('./config/database')
+const userRouter = require('./routes/auth');
+const studentRouter = require('./routes/student');
+const teacherRouter = require('./routes/teacher');
+const challengeRouter = require('./routes/challenge'); 
+const quizRouter = require('./routes/quiz');
+const reportRouter=require('./routes/reports');
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/v1/user',userRouter)
+app.use('/api/v1/student',studentRouter)
+app.use('/api/v1/challenge',challengeRouter)      
+app.use('/api/v1/teacher',teacherRouter)
+app.use('/api/v1/quiz',quizRouter)      
+app.use('/api/v1/reports',reportRouter) 
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
